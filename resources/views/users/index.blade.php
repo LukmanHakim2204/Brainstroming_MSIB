c3a5968532eaf88b657e98a8bf179b5da380ff23
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-09-26 13:05:01 +0700

    merubah judul home

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,8 +8,11 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Data Users</title>
     <style>
+        h1 { 
+            text-align: center;
+        }
         table {
             font-family: arial, sans-serif;
             border-collapse: collapse;
