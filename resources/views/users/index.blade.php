529b1b300254aa15258b2355af2a5d9282e92143
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-09-26 13:18:19 +0700

    Memperbaiki tampilan tabel

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,33 +7,12 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Data Users</title>
-    <style>
-        h1 { 
-            text-align: center;
-        }
-        table {
-            font-family: arial, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        td,
-        th {
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-
-        tr:nth-child(even) {
-            background-color: #dddddd;
-        }
-    </style>
   </head>
   <body>
     <div class="container">
-        <h1>Data Users</h1>
+        <h1>Data Users </h1>
         <table>
             <tr>
                 <th>Id User</th>
