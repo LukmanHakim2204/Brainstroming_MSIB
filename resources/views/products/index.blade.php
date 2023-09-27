90c65045c1bdc5ed8667d77435c754db5386f00d
Alamsyah703 <21082010249@student.upnjatim.ac.id>
2023-09-27 10:20:14 +0700

    add product

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,39 @@
+@extends('component.app')
+@section('content')
+    <body>
+        <div class="container">
+            <h1>Data Products </h1>
+            <table>
+                <tr>
+                    <th>Id Product</th>
+                    <th>Product Name</th>
+                    <th>Price</th>
+                    <th>Stock</th>
+                    <th>Create At</th>
+
+                </tr>
+                @foreach ($products as $product)
+                    <tr>
+                        <td>{{ $product->id_product }}</td>
+                        <td>{{ $product->product_name }}</td>
+                        <td>{{ $product->price }}</td>
+                        <td>{{ $product->stock }}</td>
+                        <td>{{ $product->created_at }}</td>
+                    </tr>
+                @endforeach
+            </table>
+            <div>
+                Showing
+                {{ $products->firstItem() }}
+                of
+                {{ $products->lastItem() }}
+                to
+                {{ $products->total() }}
+                entries
+            </div>
+            <div class="d-flex flex-row-reverse ">
+                {{ $products->links() }}
+            </div>
+        </div>
+    </body>
+@endsection
